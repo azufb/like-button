a1c9a1b2c0376b05fdf224e6d885c0301ecfd6d6
azufb <curiousazunas@gmail.com>
2021-05-15 17:21:12 +0900

    functionになっていたので、constに修正

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import LikeButton from "./component/LikeButton"
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
